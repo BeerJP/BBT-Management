ad0165b90f9735d295104a1d105a41275500f4ef
Apoliaz <scasifer@gmail.com>
2023-03-13 21:50:56 +0700

    Update app.js File

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const mysql = require("mysql");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello World !");
+const db = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "rsw_management"
 });
 
-app.use(express.static(__dirname + "/public"));
 
 app.listen(5000, () => {
-    console.log('Server started successfully');
+    console.log("Yey, your server is running on port 3001");
 });
+// app.get('/', (req, res) => {
+//     res.send("Hello World !");
+// });
+
+// app.use(express.static(__dirname + "/public"));
+
+// app.listen(5000, () => {
+//     console.log('Server started successfully');
+// });
