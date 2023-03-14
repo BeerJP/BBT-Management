adbe25a5df1f2613e0e5c7c85c7f5e7eeae1ec94
Apoliaz <scasifer@gmail.com>
2023-03-14 16:04:40 +0700

    Add Login

--- back-end/app.js
+++ back-end/app.js
@@ -1,28 +1,161 @@
 const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
 const mysql = require("mysql");
 const cors = require("cors");
 
-const app = express();
-app.use(cors());
-app.use(express.json());
 
-const db = mysql.createConnection({
+const conn = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
   password: "",
   database: "rsw_management"
 });
 
+const app = express();
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+// -------------------- เข้าสู่ระบบ --------------------
+app.post('/login', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+// -------------------- ตรวจสอบสถานะการเข้าสู่ระบบ --------------------
+app.get('/session', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+
+// -------------------- แสดงข้อมูล --------------------
+// แสดงข้อมูล Department
+app.get('/department', (request, response) => {
+  conn.query("SELECT * FROM DEPARTMENT", (err, result) => {
+    response.send(result);
+  });
+});
+
+// แสดงข้อมูล Employee
+app.get('/employee', (request, response) => {
+  conn.query("SELECT * FROM EMPLOYEE", (err, result) => {
+    response.send(result);
+  });
+});
+
+// แสดงข้อมูล User
+app.get('/user', (request, response) => {
+  conn.query("SELECT * FROM USER", (err, result) => {
+    response.send(result);
+  });
+});
+
+// แสดงข้อมูล User Type
+app.get('/type', (request, response) => {
+  conn.query("SELECT * FROM TYPE", (err, result) => {
+    response.send(result);
+  });
+});
+
+// แสดงข้อมูล Leave Day
+app.get('/leave', (request, response) => {
+  conn.query("SELECT * FROM LEAVE_DAY", (err, result) => {
+    response.send(result);
+  });
+});
+
+// แสดงข้อมูล Workday
+app.get('/workday', (request, response) => {
+  conn.query("SELECT * FROM WORKDAY", (err, result) => {
+    response.send(result);
+  });
+});
+
+// แสดงข้อมูล Holiday
+app.get('/holiday', (request, response) => {
+  conn.query("SELECT * FROM HOLIDAY", (err, result) => {
+    response.send(result);
+  });
+});
+
+// แสดงข้อมูล Time Attendance
+app.get('/time', (request, response) => {
+  conn.query("SELECT * FROM TIME_ATTENDANCE", (err, result) => {
+    response.send(result);
+  });
+});
+
+
+// -------------------- เพิ่มข้อมูล --------------------
+
+// app.post("/create", (request, response) => {
+//   const name = req.body.name;
+//   const age = req.body.age;
+//   const country = req.body.country;
+//   const position = req.body.position;
+//   const wage = req.body.wage;
+
+//   conn.query(
+//     "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+//     [name, age, country, position, wage],
+//     (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         response.send("Values Inserted");
+//       }
+//     }
+//   );
+// });
+
+
+// -------------------- แก้ไขข้อมูล --------------------
+
+// app.put("/update", (request, response) => {
+//   const id = req.body.id;
+//   const wage = req.body.wage;
+//   conn.query(
+//     "UPDATE employees SET wage = ? WHERE id = ?",
+//     [wage, id],
+//     (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         response.send(result);
+//       }
+//     }
+//   );
+// });
+
 
 app.listen(5000, () => {
-    console.log("Yey, your server is running on port 3001");
+  console.log("Yey, your server is running on port 5000");
 });
-// app.get('/', (req, res) => {
-//     res.send("Hello World !");
-// });
-
-// app.use(express.static(__dirname + "/public"));
-
-// app.listen(5000, () => {
-//     console.log('Server started successfully');
-// });
---
+++ db/demo/rsw_management.sql
@@ -0,0 +1,246 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 14, 2023 at 10:03 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rsw_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `dept_id` int(1) NOT NULL,
+  `dept_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`dept_id`, `dept_name`) VALUES
+(1, 'บัญชี'),
+(2, 'ธุรการ'),
+(3, 'จัดซื้อ'),
+(4, 'ฝ่ายขาย'),
+(5, 'ขนส่ง'),
+(6, 'สโตร์');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `emp_id` int(4) NOT NULL,
+  `emp_name` varchar(50) NOT NULL,
+  `emp_surname` varchar(50) NOT NULL,
+  `emp_idcard` varchar(13) NOT NULL,
+  `emp_gender` varchar(6) NOT NULL,
+  `emp_birthdate` date NOT NULL,
+  `emp_address` varchar(255) DEFAULT NULL,
+  `emp_status` tinyint(1) NOT NULL DEFAULT 1,
+  `emp_startdate` date NOT NULL,
+  `emp_enddate` date DEFAULT NULL,
+  `emp_mac1` varchar(17) DEFAULT NULL,
+  `emp_mac2` varchar(17) DEFAULT NULL,
+  `dept_id` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`emp_id`, `emp_name`, `emp_surname`, `emp_idcard`, `emp_gender`, `emp_birthdate`, `emp_address`, `emp_status`, `emp_startdate`, `emp_enddate`, `emp_mac1`, `emp_mac2`, `dept_id`) VALUES
+(1001, 'จักรพันธ์', 'ภูพาพุทธ', '1470900098834', 'ชาย', '1995-05-23', NULL, 1, '2013-05-23', NULL, NULL, NULL, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `holiday`
+--
+
+CREATE TABLE `holiday` (
+  `holi_name` varchar(50) NOT NULL,
+  `work_id` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leave_day`
+--
+
+CREATE TABLE `leave_day` (
+  `leave_type` varchar(10) NOT NULL,
+  `leave_date` date NOT NULL,
+  `leave_description` varchar(255) NOT NULL,
+  `leave_appove` tinyint(1) NOT NULL DEFAULT 0,
+  `emp_id` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_attendance`
+--
+
+CREATE TABLE `time_attendance` (
+  `time_in` time NOT NULL,
+  `time_out` time DEFAULT NULL,
+  `work_id` varchar(8) NOT NULL,
+  `emp_id` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type`
+--
+
+CREATE TABLE `type` (
+  `type_id` int(1) NOT NULL,
+  `type_name` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`type_id`, `type_name`) VALUES
+(1, 'Admin'),
+(2, 'HR'),
+(3, 'User');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_name` varchar(12) NOT NULL,
+  `user_password` varchar(12) NOT NULL,
+  `type_id` int(1) NOT NULL,
+  `emp_id` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_name`, `user_password`, `type_id`, `emp_id`) VALUES
+('jakkapan', '23052538', 1, 1001);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `workday`
+--
+
+CREATE TABLE `workday` (
+  `work_id` varchar(8) NOT NULL,
+  `work_date` date NOT NULL,
+  `work_status` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`dept_id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`emp_id`),
+  ADD KEY `dept_id` (`dept_id`);
+
+--
+-- Indexes for table `holiday`
+--
+ALTER TABLE `holiday`
+  ADD KEY `work_id` (`work_id`);
+
+--
+-- Indexes for table `leave_day`
+--
+ALTER TABLE `leave_day`
+  ADD KEY `emp_id` (`emp_id`);
+
+--
+-- Indexes for table `time_attendance`
+--
+ALTER TABLE `time_attendance`
+  ADD KEY `work_id` (`work_id`),
+  ADD KEY `emp_id` (`emp_id`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`type_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD KEY `type_id` (`type_id`),
+  ADD KEY `emp_id` (`emp_id`);
+
+--
+-- Indexes for table `workday`
+--
+ALTER TABLE `workday`
+  ADD PRIMARY KEY (`work_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `dept_id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `emp_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1002;
+
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `type_id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
