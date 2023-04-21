ee73e2d487b8e7e50c64e1789fec859190268e1c
Apoliaz <scasifer@gmail.com>
2023-04-21 16:22:01 +0700

    21-04-2566 V.1

--- client/src/components/report/report_content.js
+++ client/src/components/report/report_content.js
@@ -1,22 +1,29 @@
-import { React, useState, useEffect } from 'react';
+import { React, useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
 
 import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
 import Paper from '@mui/material/Paper';
 import AppBar from "@mui/material/AppBar";
-import Button from '@mui/material-next/Button';
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import SearchIcon from '@mui/icons-material/Search';
+
+import Button from '@mui/material-next/Button';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import PropTypes from 'prop-types';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 
 function Content(props) {
@@ -26,7 +33,6 @@ function Content(props) {
     const [isReportyear, setReportyear] = useState(0);
     const [isReportmont, setReportmont] = useState(0);
     const [isReportdept, setReportdept] = useState(0);
-
     const [isReportview, setReportview] = useState('');
 
     const deptChange = (event) => {
@@ -40,6 +46,10 @@ function Content(props) {
     const montChange = (event) => {
         setReportmont(event.target.value);
     };
+
+    useEffect(() => {
+        
+    })
 
     function setDefault() {
         setReportyear('')
@@ -61,94 +71,127 @@ function Content(props) {
         p: 4,
     };
 
+    function createData(name, calories, fat, carbs, protein, price) {
+        return {
+            name,
+            calories,
+            fat,
+            carbs,
+            protein,
+            price,
+            history: [
+            {
+                id: '1001',
+                name: 'จักรพันธ์ ภูพาพุทธ',
+                customerId: '8.37 น.',
+                amount: '17.00 น.',
+            },
+            {
+                id: '1002',
+                name: 'สมชาย อัคเคอ',
+                customerId: '8.37 น.',
+                amount: '17.00 น.',
+            },
+            ],
+        };
+    }
+
+    function Row(props) {
+        const { row } = props;
+        const [open, setOpen] = useState(false);
+      
+        return (
+            <Fragment>
+                <TableRow sx={{ '& > *': { borderBottom: 'unset' }}}>
+                    <TableCell align="center">{row.name}</TableCell>
+                    <TableCell align="center">{row.calories}</TableCell>
+                    <TableCell align="center">{row.fat}</TableCell>
+                    <TableCell align="center">{row.carbs}</TableCell>
+                    <TableCell align="center">{row.protein}</TableCell>
+                    <TableCell align="center">
+                        <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                        >
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                        </IconButton>
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            <Box sx={{ margin: 'auto', width: 900 }}>
+                                <Table aria-label="collapsible table">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell align="center">รหัส</TableCell>
+                                            <TableCell align="center">ชื่อ - นามสกุล</TableCell>
+                                            <TableCell align="center">เวลาเข้างาน</TableCell>
+                                            <TableCell align="center">เวลาออกงาน</TableCell>
+                                            <TableCell align="center">สถานะ</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {
+                                            row.history.map((historyRow) => (
+                                                <TableRow key={historyRow.id}>
+                                                    <TableCell align="center">{historyRow.id}</TableCell>
+                                                    <TableCell align="center">{historyRow.name}</TableCell>
+                                                    <TableCell align="center">{historyRow.customerId}</TableCell>
+                                                    <TableCell align="center">{historyRow.amount}</TableCell>
+                                                    <TableCell align="center">ปกติ</TableCell>
+                                                </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </Box>
+                        </Collapse>
+                    </TableCell>
+                </TableRow>
+            </Fragment>
+        );
+    }
+
+    Row.propTypes = {
+        row: PropTypes.shape({
+          calories: PropTypes.number.isRequired,
+          carbs: PropTypes.number.isRequired,
+          fat: PropTypes.number.isRequired,
+          history: PropTypes.arrayOf(
+            PropTypes.shape({
+              amount: PropTypes.number.isRequired,
+              customerId: PropTypes.string.isRequired,
+              name: PropTypes.string.isRequired,
+              id: PropTypes.string.isRequired,
+            }),
+          ).isRequired,
+          name: PropTypes.string.isRequired,
+          price: PropTypes.number.isRequired,
+          protein: PropTypes.number.isRequired,
+        }).isRequired,
+    };
+      
+    const rows = [
+        createData('03-01-2566', 5, 5, 0, 0, 1),
+        createData('04-01-2566', 5, 3, 2, 0, 1),
+        createData('05-01-2566', 5, 5, 0, 0, 1),
+        createData('06-01-2566', 4, 4, 0, 1, 1),
+        createData('07-01-2566', 5, 4, 1, 0, 1),
+        createData('09-01-2566', 5, 5, 0, 0, 1),
+        createData('10-01-2566', 5, 3, 2, 0, 1),
+        createData('11-01-2566', 5, 5, 0, 0, 1),
+        createData('12-01-2566', 4, 4, 0, 1, 1),
+        createData('13-01-2566', 5, 4, 1, 0, 1),
+        createData('14-01-2566', 5, 5, 0, 0, 1),
+        createData('16-01-2566', 5, 3, 2, 0, 1),
+        createData('17-01-2566', 5, 5, 0, 0, 1),
+        createData('18-01-2566', 4, 4, 0, 1, 1),
+        createData('19-01-2566', 5, 4, 1, 0, 1),
+    ];
+
     return(
         <>  
-            {/* <Modal
-            open={isOpen}
-            onClose={() => setClose()}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <Box sx={{ minWidth: 120 }}>
-                        <FormControl fullWidth>
-                            <InputLabel id="demo-simple-select-label" sx={{ background: 'white' }}>รายงาน&nbsp;</InputLabel>
-                            <Select
-                            sx={{ mb: 3 }}
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={isReporttype}
-                            label="Age"
-                            onChange={typeChange}
-                            >
-                                <MenuItem value={1}>ทั้งหมด</MenuItem>
-                                <MenuItem value={2}>รายงานสรุป</MenuItem>
-                                <MenuItem value={3}>รายการย่อย</MenuItem>
-                            </Select>
-                        </FormControl>
-                        <FormControl fullWidth>
-                            <InputLabel id="demo-simple-select-label" sx={{ background: 'white' }}>แผนก&nbsp;</InputLabel>
-                            <Select
-                            sx={{ mb: 3 }}
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={isReportdept}
-                            label="Age"
-                            onChange={deptChange}
-                            >
-                                <MenuItem value={1}>ทั้งหมด</MenuItem>
-                                <MenuItem value={2}>ออฟฟิศ 1</MenuItem>
-                                <MenuItem value={3}>ออฟฟิศ 2</MenuItem>
-                                <MenuItem value={4}>สโตร์ & ขนส่ง</MenuItem>
-                            </Select>
-                        </FormControl>
-                        <FormControl fullWidth>
-                            <InputLabel id="demo-simple-select-label" sx={{ background: 'white' }}>ขอบเขต&nbsp;</InputLabel>
-                            <Select
-                            sx={{ mb: 3 }}
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={isReportrows}
-                            label="Age"
-                            onChange={rowsChange}
-                            >
-                                <MenuItem value={1}>ทั้งหมด</MenuItem>
-                                <MenuItem value={2}>โดยปี</MenuItem>
-                                <MenuItem value={3}>โดยเดือน</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </Box>
-                    {
-                        isReportrows === 2 ? 
-                        <Box sx={{ minWidth: 120, display: 'flex', justifyContent: 'space-between' }}>
-                            <LocalizationProvider dateAdapter={AdapterMoment}>
-                                <DatePicker sx={{width: 185}} views={['year']}/>
-                            </LocalizationProvider>
-                            <label className='date-picker-label'>-</label>
-                            <LocalizationProvider dateAdapter={AdapterMoment}>
-                                <DatePicker sx={{width: 185}} views={['year']}/>
-                            </LocalizationProvider>
-                        </Box>
-                        :
-                        isReportrows === 3 ? 
-                        <Box sx={{ minWidth: 120, display: 'flex', justifyContent: 'space-between' }}>
-                            <LocalizationProvider dateAdapter={AdapterMoment}>
-                                <DatePicker sx={{width: 185}} views={['year', 'month']}/>
-                            </LocalizationProvider>
-                            <label className='date-picker-label'>-</label>
-                            <LocalizationProvider dateAdapter={AdapterMoment}>
-                                <DatePicker sx={{width: 185}} views={['year', 'month']}/>
-                            </LocalizationProvider>
-                        </Box>
-                        : ''
-                    }
-                    <Typography sx={{ mt: 3, textAlign: 'center' }} >
-                        <Button variant="outlined" color="success" size="normal" sx={{ borderRadius: 'none' }}>
-                            ตกลง
-                        </Button>
-                    </Typography>
-                </Box>
-            </Modal> */}
             <div className='report_container'>
                 <div className='report_article'>
                     <Box
@@ -169,7 +212,7 @@ function Content(props) {
                                         รายงานสรุปการทำงาน
                                     </Typography>
                                     <Typography id='modal-modal-description' sx={{ textAlign: 'center', display: 'flex', alignItems: 'center' }}>
-                                        <FormControl sx={{ width: 140 }} size="small">
+                                        {/* <FormControl sx={{ width: 140 }} size="small">
                                             <InputLabel sx={{ background: 'white' }}>&nbsp;&nbsp;&nbsp;ปี</InputLabel>
                                             <Select
                                             sx={{ mr: 1 }}
@@ -180,7 +223,7 @@ function Content(props) {
                                                 <MenuItem value={0}>ทั้งหมด</MenuItem>
                                                 <MenuItem value={2566}>2566</MenuItem>
                                             </Select>
-                                        </FormControl>
+                                        </FormControl> */}
                                         <FormControl sx={{ width: 140 }} size="small">
                                             <InputLabel sx={{ background: 'white' }}>&nbsp;เดือน</InputLabel>
                                             <Select
@@ -204,20 +247,6 @@ function Content(props) {
                                                 <MenuItem value={12}>ธันวาคม</MenuItem>
                                             </Select>
                                         </FormControl>
-                                        <FormControl sx={{ width: 150 }} size="small">
-                                            <InputLabel sx={{ background: 'white' }}>แผนก&nbsp;</InputLabel>
-                                            <Select
-                                            sx={{ mr: 1 }}
-                                            value={isReportdept}
-                                            label="Type"
-                                            onChange={deptChange}
-                                            >
-                                                <MenuItem value={0}>ทั้งหมด</MenuItem>
-                                                <MenuItem value={1}>ออฟฟิศ 1</MenuItem>
-                                                <MenuItem value={2}>ออฟฟิศ 2</MenuItem>
-                                                <MenuItem value={3}>สโตร์ & ขนส่ง</MenuItem>
-                                            </Select>
-                                        </FormControl>
                                     </Typography>
                                 </Toolbar>
                             </AppBar>
@@ -238,17 +267,30 @@ function Content(props) {
                             lg: 820
                         },
                         },
-                        // '&::-webkit-scrollbar': {
-                        //     width: '5px'
-                        //   }
                     }}
                     >
                         <Paper elevation={1} sx={{ display: 'flex', flexDirection: 'column'}}>
-                            {
-                                isReportview === '' ? ''
-                                :
-                                <Paper elevation={1} sx={{ display: 'flex'}}><h4>รายงานวันที่ {isReportview.start} - {isReportview.end}</h4></Paper>
-                            }
+                            <TableContainer>
+                                <Table aria-label="collapsible table">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell align="center">วันที่</TableCell>
+                                            <TableCell align="center">ใบบันทึกเวลา</TableCell>
+                                            <TableCell align="center">เข้างานปกติ</TableCell>
+                                            <TableCell align="center">เข้างานสาย</TableCell>
+                                            <TableCell align="center">ใบลา</TableCell>
+                                            <TableCell />
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                    {
+                                        rows.map((row) => (
+                                            <Row key={row.name} row={row} />
+                                        ))
+                                    }
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
                         </Paper>
                     </Box>
                 </div>
